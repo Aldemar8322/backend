d51f3e0ed0552403125577d5f9c7872fe8fa28ad
Aldemar8322 <162385518+Aldemar8322@users.noreply.github.com>
2024-05-30 19:07:43 -0500

    proyecto completo

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "dotenv/config";
+import "./conexionBD.js";
+import servidor from "./servidor.js";
+
+servidor.listen(3000, ()=>{
+    console.log("Servidor corriendo en el puerto 3000");
+});
---
+++ src/modelos/modeloUsuario.js
@@ -0,0 +1,10 @@
+import { Schema, model } from "mongoose";
+
+const esquemaUsuario = new Schema(
+    {
+        nombre: {type: String, required: true},
+        correoElectronico: {type: String, required: true},
+        contrasenia: {type: String, required: true},
+})
+
+export default model("Usuario", esquemaUsuario);
